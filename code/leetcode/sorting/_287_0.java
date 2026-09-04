6f052041f42756632070faab3419dc8450d1f685
FLASH <mr.azeem.sir@gmail.com>
2026-09-04 18:32:25 +0530

    Create _287_0.java

---
+++ code/leetcode/sorting/_287_0.java
@@ -0,0 +1,17 @@
+package leetcode.sorting;
+
+import java.util.Scanner;
+
+public class _287_0 {
+    public static void main(String[] args){
+        Scanner a=new Scanner(System.in);
+        System.out.println("Enter length of array to input");
+        int l=a.nextInt();
+        int[] nums=new int[l];
+        System.out.println("enter array element one by one");
+        for(int i=0;i<l;i++){
+            nums[i]=a.nextInt();
+        }
+        
+    }
+}
